5a29e308fe1ed6a73676b74c766cb465cca9636b
Magnus Løwer <148643539+magnus-lower@users.noreply.github.com>
2026-09-17 17:41:28 +0200

    vairables

--- julian_magnus/main.tf
+++ julian_magnus/main.tf
@@ -1,25 +1,23 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "ecocomputetfstate1234"
-    container_name       = "tfstate"
-    key                  = "ecocompute.tfstate"
-    use_azuread_auth = true
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "example" {
-  name     = "EcoCompute-test123"
-  location = "austriaeast"
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+    subnet {
+      name           = "subnet1"
+      address_prefix = "10.0.0.0/24"
+      security_group = azurerm_network_security_group.example.id
+  }
 }
